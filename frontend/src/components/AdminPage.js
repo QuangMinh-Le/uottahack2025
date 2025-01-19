e43063e4f8ad969ef3755841ecd8bf62ef27655d
Minh <quangminh.ottawa@gmail.com>
2025-01-19 08:16:07 -0500

    update client page

---
+++ frontend/src/components/AdminPage.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import solace from 'solclientjs';
+
+import BarChart from './BarChartAdmin';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './AdminPage.css';
+
+
+
+const AdminPage = (props) => {
+
+    const dataset = {
+        "label":"Washroom Traffic",
+        "data": [12, 19, 3, 5, 2, 3]
+    }
+    const labels = ["Toilet 1", "Toilet 2", "Toilet 3", "Toilet 4", "Toilet 5"]
+
+    // const [chartData, setChartData] = useState({});
+    // const washrooms = [
+    //     { id: 1, time: "2025-01-18T01:23:45Z" },
+    //     { id: 2, time: "2025-01-18T01:45:30Z" },
+    //     { id: 3, time: "2025-01-18T02:15:20Z" },
+    //     { id: 4, time: "2025-01-18T03:05:10Z" },
+    //     { id: 5, time: "2025-01-18T01:55:00Z" },
+    //     { id: 6, time: "2025-01-18T01:55:00Z" },
+    //     { id: 5, time: "2025-01-18T01:55:00Z" },
+    //     { id: 1, time: "2025-01-18T01:55:00Z" },
+    //     { id: 2, time: "2025-01-18T01:55:00Z" },
+    //     { id: 6, time: "2025-01-18T01:55:00Z" },
+    //     { id: 2, time: "2025-01-18T01:55:00Z" },
+    //     { id: 2, time: "2025-01-18T01:55:00Z" },
+    // ];
+    // useEffect(() => {
+    //     const hourlyCounts = filterAndCountByHour(washrooms);
+
+    //     setChartData({
+    //         labels: Array.from({ length: 24 }, (_, i) => `${i}:00 - ${i + 1}:00`),
+    //         datasets: [
+    //             {
+    //                 label: "Objects Count Per Hour",
+    //                 data: hourlyCounts,
+    //                 backgroundColor: "rgba(75, 192, 192, 0.6)",
+    //                 borderColor: "rgba(75, 192, 192, 1)",
+    //                 borderWidth: 1,
+    //             },
+    //         ],
+    //     });
+    // }, [washrooms]);
+    // // Function to process dataset
+    // const filterAndCountByHour = (data) => {
+    //     const hourlyCounts = Array(24).fill(0);
+    //     data.forEach((item) => {
+    //         const hour = new Date(item.time).getHours();
+    //         hourlyCounts[hour]++;
+    //     });
+    //     return hourlyCounts;
+    // };
+    return (
+        <div className="container">
+            This is admin page
+            <BarChart dataset={dataset} labels={labels}/>
+            {/* <BarChart dataset={dataset} labels={chartData.labels}/> */}
+        </div>
+    );
+};
+
+export default AdminPage;
--- frontend/src/components/ClientPage.js
+++ frontend/src/components/ClientPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import solace from 'solclientjs';
+import video from './assets/Kitten Unrolling Toilet Paper.mp4';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './ClientPage.css';
 import Washroom from './Washroom';
+import { BrowserRouter } from "react-router-dom";
 
 let data = "";
 let totalTaken = 0;
@@ -141,7 +143,11 @@ const ClientPage = (props) => {
         }, []);
     
     return (
+        <BrowserRouter>
+        
         <div className="container-fluid">
+
+         <iframe href="https://www.mappedin.com/" title="Mappedin Map" name="Mappedin Map" allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com" scrolling="no" width="100%" height="400" frameborder="0" src="https://app.mappedin.com/map/678c2b4c041860000bcf72cb?embedded=true"></iframe>
             {/* Top Half: Floorplan Image */}
             <div className="row">
                 <div className="col-12">
@@ -184,9 +190,17 @@ const ClientPage = (props) => {
                             ))}
                         </tbody>
                     </table>
+                    {/* Center the video */}
+                    <div className="d-flex justify-content-center my-4">
+                        <video autoPlay muted loop id="myVideo" style={{ maxWidth: "100%", height: "auto" }}>
+                            <source src={video} type="video/mp4" />
+                            Your browser does not support HTML5 video.
+                        </video>
+                    </div>
                 </div>
             </div>
         </div>
+        </BrowserRouter>
     );
 };
 
