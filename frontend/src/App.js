92ef02123b74eca808e228923035a6238f07dea7
tung <thanhtungtran112233@gmail.com>
2025-01-19 06:52:33 -0500

    Updated admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,14 @@
-// App.js
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
-
-import ClientPage from "./components/ClientPage";
-import AdminDashboard from "./AdminPage/AdminPage";
-
+import React from 'react';
+import ClientPage from './components/ClientPage';
+import AdminDashboard from './AdminPage/AdminPage';
 function App() {
-  return (
-    <Router>
-      {/* Basic navigation bar */}
-      <nav style={{ padding: "1rem", backgroundColor: "#e9ecef" }}>
-        <Link to="/client" style={{ marginRight: "1rem" }}>
-          Client
-        </Link>
-        <Link to="/admin">Admin</Link>
-      </nav>
+    return(
+    <div>
+        <AdminDashboard />;
+        <ClientPage />;
+    </div>
 
-      {/* Define routes */}
-      <Routes>
-        <Route path="/client" element={<ClientPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-
-        {/* Redirect any unknown route to /client */}
-        <Route path="*" element={<Navigate to="/client" replace />} />
-      </Routes>
-    </Router>
-  );
+    ) 
 }
 
 export default App;
